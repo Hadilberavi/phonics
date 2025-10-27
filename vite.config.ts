80ee0e51f9dba34be03714c84aa7fda32eb5a0de
HADEL BERAWİ <berawihadel@gmail.com>
2025-10-28 02:18:42 +0300

    add all pages to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   root: "src",
   base: "/phonics/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.html"),
+        contact: resolve(__dirname, "src/contact.html"),
+        cources: resolve(__dirname, "src/courses.html"),
+        resources: resolve(__dirname, "src/Resources.html"),
+        workshops: resolve(__dirname, "src/workshops.html"),
+      },
+    },
+  },
+
   plugins: [tailwindcss()],
 });
